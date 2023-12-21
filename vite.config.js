4f66e5ff51fddb15f155b4556ec010cae696dbe4
tmwebste <tmwebste@andrew.cmu.edu>
2023-12-21 11:15:38 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Tyler_Webster_Portfolio/"
-  base: "."
+  base: "/Tyler_Webster_Portfolio/"
+  // base: "."
 })
